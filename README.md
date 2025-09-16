526bcb875647cafd0e2e31f71a1696937acd771b
deVerick06 <elianeheri261206@gmail.com>
2025-09-16 09:26:05 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,26 @@
+# Amigo Secreto
+Um sistema web que permite adicionar amigos, sortear um amigo secreto aleatoriamente e reiniciar a lista para um novo sorteio.
+
+## Como executar:
+1. Baixe os arquivos do projeto.
+2. Abra o arquivo 'index.html'.
+3. Insira os nomes, clique em Adicionar e logo após Sortear.
+4. Para começar um novo sorteio, clique em Resetar
+
+## Estrutura do Projeto:
+- index.html -> Estrutura HTML do sistema / Esqueleto
+- style.css -> Estilização do sistema
+- app.js -> Lógica do sistema usando JS
+- assets/ -> Imagens usadas no projeto
+
+## Dependências:
+Nenhuma dependencia externa, o sistema pode ser rodado em qualquer navegador moderno.
+
+## Funcionalidades:
+- Adicionar nomes na lista
+- Sortear um nome dentro da lista
+- Resetar lista
+
+## Possiveis melhorias futuras:
+- Evitar duplicatas no sorteio
+- Melhorar a interface para dispositivos móveis
--- app.js
+++ app.js
@@ -1,13 +1,46 @@
-let nomes_amigos = [];
+let nomes_amigos = []; 
 
-function adicionarAmigo() {
-    //pegar a entrada do usuario la no HTML
-    //criar um loop
-        //if estiver vazio:
-            //alertar
-            //mandar inserir valido
-        //else:
-            //OK
-            //adicionar no nomes_amigos
-            //limpar campo para a proxima entrada
+function adicionarAmigo() { 
+    let campoNome = document.getElementById("amigo");
+    let nome = campoNome.value;
+    if (nome == ""){
+        alert("Insira um nome!");
+        return;
+    }
+    else{
+        nomes_amigos.push(nome);
+        atualizarLista();
+        campoNome.value = "";
+    }
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ""
+    for (const nomes of nomes_amigos) {
+        lista.innerHTML += "<li>" + nomes + "</li>"
+    }
+}
+
+function sortearAmigo() {
+    if (nomes_amigos.length == 0){
+        alert("Nenhum nome foi inserido!");
+        return;
+    }
+    else{
+        let indice = Math.floor(Math.random()*nomes_amigos.length);
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = nomes_amigos[indice]
+    }
+}
+
+function resetarLista() {
+    if (!confirm("Deseja realmente limpar a lista de nomes?")) {
+        return; 
+    }
+    nomes_amigos = [];
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = "";
 }
